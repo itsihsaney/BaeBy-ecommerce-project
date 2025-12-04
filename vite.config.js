671bfbe0530207662ccdc4dacb6ce954a0f79e89
itsihsaney <muhammedihsan0010@gmail.com>
2025-12-04 11:11:31 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),       
+    tailwindcss(), 
   ],
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+  }
 })
